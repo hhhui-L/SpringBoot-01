18ea7c4aa113dc0265fb6d20812e6f9299ce120b
hhhui-L <1184131956@qq.com>
2021-04-12 22:46:21 +0800

    04.12

--- springboot-01-helloworld/src/main/java/com/lgh/boot/MainApplication.java
+++ springboot-01-helloworld/src/main/java/com/lgh/boot/MainApplication.java
@@ -1,5 +1,6 @@
 package com.lgh.boot;
 
+import ch.qos.logback.core.db.DBHelper;
 import com.lgh.boot.bean.Pet;
 import com.lgh.boot.bean.User;
 import com.lgh.boot.config.MyConfig;
@@ -11,7 +12,7 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
- * 主程序类
+ * 主程序类;主配置类
  * * @SpringBootApplication：这是一个SpringBoot应用
  * scanBasePackages来指定扫描的基础包就是com.cky,也就是将整个包层级放大一点，让它来进行扫描
  */
@@ -32,24 +33,42 @@ public class MainApplication {
         for(String name:names){
             System.out.println(name);
         }
-        //3.从容器中获取组件
-        Pet tom01=run.getBean("tom", Pet.class);
-        Pet tom02=run.getBean("tom",Pet.class);
-        System.out.println("组件："+(tom01==tom02));
+//        //3.从容器中获取组件
+//        Pet tom01=run.getBean("tom", Pet.class);
+//        Pet tom02=run.getBean("tom",Pet.class);
+//        System.out.println("组件："+(tom01==tom02));
+//
+//        //4.com.lgh.boot.config.MyConfig$$EnhancerBySpringCGLIB$$beeca609@5b6e8f77
+//        MyConfig bean = run.getBean(MyConfig.class);
+//        System.out.println(bean);
+//
+//        //如果@Configuration(proxyBeanMethods = true)代理对象调用方法。SpringBoot总会检查这个组件是否在容器中有
+//        //保持组件单实例
+//        User user = bean.user01();
+//        User user1 = bean.user01();
+//        System.out.println(user==user1);
+//
+//        User user01 = run.getBean("user01", User.class);
+//        Pet tom = run.getBean("tom", Pet.class);
+//        System.out.println("用户的宠物："+(user01.getPet()==tom));
 
-        //4.com.lgh.boot.config.MyConfig$$EnhancerBySpringCGLIB$$beeca609@5b6e8f77
-        MyConfig bean = run.getBean(MyConfig.class);
-        System.out.println(bean);
+        //5.获取组件
+//        String[] beanNamesForType = run.getBeanNamesForType(User.class);
+//        System.out.println("-----------------");
+//        for(String s:beanNamesForType){
+//            System.out.println(s);
+//        }
+//        DBHelper bean1 = run.getBean(DBHelper.class);
+//        System.out.println(bean1);
 
-        //如果@Configuration(proxyBeanMethods = true)代理对象调用方法。SpringBoot总会检查这个组件是否在容器中有
-        //保持组件单实例
-        User user = bean.user01();
-        User user1 = bean.user01();
-        System.out.println(user==user1);
+        boolean tom = run.containsBean("tom");
+        System.out.println("容器中Tom组件"+tom);
 
-        User user01 = run.getBean("user01", User.class);
-        Pet tom = run.getBean("tom", Pet.class);
-        System.out.println("用户的宠物："+(user01.getPet()==tom));
+        boolean user01 = run.containsBean("user01");
+        System.out.println("容器中user01组件"+user01);
 
+
+        boolean tom22 = run.containsBean("tom22");
+        System.out.println("容器中tom22组件"+tom22);
     }
 }
--- springboot-01-helloworld/src/main/java/com/lgh/boot/config/MyConfig.java
+++ springboot-01-helloworld/src/main/java/com/lgh/boot/config/MyConfig.java
@@ -1,9 +1,14 @@
 package com.lgh.boot.config;
 
+import ch.qos.logback.core.db.DBHelper;
 import com.lgh.boot.bean.Pet;
 import com.lgh.boot.bean.User;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.security.ConditionalOnDefaultWebSecurity;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 /**
  * 1、配置类里面使用@Bean标注在方法上给容器注册组件，默认也是单实例的
@@ -12,9 +17,14 @@ import org.springframework.context.annotation.Configuration;
  *   Full(proxyBeanMethods = true)
  *   Lite(proxyBeanMethods = false)
  *   组件依赖必须使用Full模式默认。其他默认是否Lite模式
+ * 4、@Import({User.class, DBHelper.class})
+ *   给容器中自动创建出这两个类型的组件、默认组件的名字就是全类名
  *
  */
+@Import({User.class, DBHelper.class}) // // DBHelper.class 为第三方 jar 包中的类
 @Configuration(proxyBeanMethods = true) //告诉SpringBoot这是一个配置类，等同于配置文件
+//@ConditionalOnBean(name="tom") //标注在类上，含有tom组件，以下才生效
+@ConditionalOnMissingBean(name="tom") //不含有tom组件，以下才生效
 public class MyConfig {
 
     /**
@@ -22,6 +32,7 @@ public class MyConfig {
      * @return
      */
 
+//    @ConditionalOnBean(name="tom") //条件装配注解，含有tom组件才生效
     @Bean //给容器中添加组件，以方法名作为组件的id，返回类型就是组件类型，返回的值就是组件在容器中的实例
     public User user01(){
         User zhangsan=new User("zhangsan",18);
@@ -30,7 +41,7 @@ public class MyConfig {
         return zhangsan;
     }
 
-    @Bean("tom") //如果不想让方法名作为组件名也可以在Bean标签直接给一个自定义的名字
+    @Bean("tom22") //如果不想让方法名作为组件名也可以在Bean标签直接给一个自定义的名字
     public Pet tomcatPet(){
         return new Pet("tomcat");
     }
